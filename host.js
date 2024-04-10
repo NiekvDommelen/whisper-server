0341d9db5591c9779b3208fae14e7fff2468413d
SneakyNieky <niekdommelen37@gmail.com>
2024-04-10 12:49:38 +0200

    handle connections

--- host.js
+++ host.js
@@ -7,3 +7,24 @@ var server = http.createServer();
 server.listen(8080, host, function () {});
 
 const wss = new WebSocket.Server({ httpServer: server, port: 3001 });
+
+wss.on("connection", (ws, req) => {
+
+    var client_adress = ws._socket.remoteAddress;
+    var client_port = ws._socket.remotePort;    
+
+
+    ws.on("message", (message) => {
+    
+        data = JSON.parse(message.toString());
+        data["timestamp"] = new Date().getTime();
+        
+
+        if(data["status"] == "connecting"){
+            clients[data["userid"]] = ws;
+            return;
+        }
+
+    });
+
+});
